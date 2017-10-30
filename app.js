e3596de35b870ea85470e57450b262fe3617ee25
zaki <zaki.sediqyar@gmail.com>
2017-10-29 20:30:13 -0700

    routes and index updated

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var express = require("express");
+var ejs = require("ejs");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-var ejs = require("ejs");
 var methodOverride = require("method-override");
 
 var app = express();
 
+var mainRoutes	= require("./routes/main");
 
-app.set("view engine, ejs");
+//Library setup
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+//Database connection in localDB
 mongoose.connect("mongodb://localhost/intranet_db");
 
 //index route
@@ -19,12 +21,26 @@ app.get("/", function(req, res){
 	res.render("index");
 });
 
+//ideas page
+app.get("/ideas", function(req, res){
+	res.render("ideas");
+});
+
+//offices page
+app.get("/offices", function(req, res){
+	res.render("offices");
+});
+
+			//sf page
+			app.get("/sf", function(req, res){
+					res.render("sf");
+			});
 
 
 
 
 
-
-var PORT = process.env.PORT || 5000;
-app.listen(5000);
-console.log("connection success");
+//PORT configuration
+app.listen(5000, () => {
+	console.log('App started on port 5000');
+});
